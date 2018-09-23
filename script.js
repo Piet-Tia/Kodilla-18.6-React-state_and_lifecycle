3ee172cd8d92509f87e8e0c104c0a3c5f8f6e5d4
Piotr Nastaj <piotr@nastaj.net>
2018-09-23 12:24:57 +0200

    Update script.js to add multiple counters

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const Counter1 = React.createClass({
+
+const counterAmount = prompt("How many counter would you like?");
+console.log(counterAmount);
+
+const Counter = React.createClass({
 	getInitialState: function () {
 		return {
 			counter: 0
@@ -11,62 +15,71 @@ const Counter1 = React.createClass({
 		});
 	},
 
-	decrement: function() {
+	decrement: function () {
 		this.setState({
 			counter: this.state.counter - 1
 		});
 	},
 
 	render: function () {
-		return React.createElement('div',{},
+		return React.createElement('div', {},
 			React.createElement('button', { onClick: this.decrement }, '-'),
-			React.createElement('span', {}, ' Licznik 1: ' + this.state.counter + ' '),
+			React.createElement('span', {}, ' Licznik ' + this.props.counterNumber + ': ' + this.state.counter + ' '),
 			React.createElement('button', { onClick: this.increment }, '+'),
 		);
 	}
 });
 
-const Counter2 = React.createClass({
-	getInitialState: function () {
-		return {
-			counter: 0
-		};
-	},
-
-	increment: function () {
-		this.setState({
-			counter: this.state.counter + 1
-		});
-	},
-
-	decrement: function() {
-		this.setState({
-			counter: this.state.counter - 1
-		});
-	},
-
-	render: function () {
-		return React.createElement('div',{},
-			React.createElement('button', { onClick: this.decrement }, '-'),
-			React.createElement('span', {}, ' Licznik 2: ' + this.state.counter + ' '),
-			React.createElement('button', { onClick: this.increment }, '+'),
-		);
-	}
-});
-
-
-//const app1 = React.createElement(Counter1);
-//const app2 = React.createElement(Counter2);
+let numbers = [];
+for (i=0 ; i < counterAmount ; i++){
+	numbers.push(i+1);
+}
 
 const App = React.createClass({
-	render: function(){
-		return React.createElement('div',{}, 
-			React.createElement(Counter1),
-			React.createElement(Counter2),
+	render: function () {
+		return React.createElement('div', {},
+			this.props.numbers.map(function(number){
+				return React.createElement(Counter, { 'counterNumber': number })
+			})
 		)
 	}
 });
 
-const app = React.createElement(App);
+const app = React.createElement(App, {numbers:numbers});
 
 ReactDOM.render(app, document.getElementById('app'));
+
+/*
+const TestCounter = React.createClass({
+	getDefaultProps: function(){
+		console.log('test1');
+	},
+	getInitialState: function(){
+		console.log('test2');
+	},
+	componentWillMount: function(){
+		console.log('test3');
+	},
+	render: function(){
+		console.log('test4');
+	},
+	componentDidMount(){
+		console.log('test5');
+	},
+	componentWillReceiveProps: function(nextProps){
+		console.log('test6');
+	},
+	shouldComponentUpdate: function(nextProps, nextState){
+		console.log('test7');
+	},
+	componentWillUpdate: function(nextProps, nextState){
+		console.log('test8');
+	},
+	componentDidUppdate: function(prevProps, prevState){
+		console.log('test9');
+	},
+	componentWillUnmount: function(){
+		console.log('test10');
+	},
+})
+*/
